7cdaa519e8f484f8298d88d5616d602da6190361
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-05-19 19:18:55 -0300

    Quarta Questão Exercicio bl4.1

--- exercises/exercises/introducao-a-javascript-e-logica-de-programacao_1/1questao/exIntroducao.js
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_1/1questao/exIntroducao.js
@@ -1,16 +1,13 @@
-// 3 Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+// 4 Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
 
-let a=70
-let b=50
-let c=40
+let valor=-6
 
-
-if(a>b && a>c){
-    console.log("a é o maior numero");
+if(valor>0){
+    console.log("positivo");
 }
-else if(b>a && b>c){
-    console.log("b é o manior numero");
+else if(valor<0){
+    console.log("negativo");
 }
 else{
-    console.log("c é o maior numero");
+    console.log("zero");
 }
