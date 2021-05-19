13fc4da891c016e26e98f4a776f7e46dc203e479
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-05-19 18:27:52 -0300

    Primeira Questão Exercicio bl4.1

---
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_1/1questao/exIntroducao.js
@@ -0,0 +1,15 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+let a=20;
+let b=50;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
