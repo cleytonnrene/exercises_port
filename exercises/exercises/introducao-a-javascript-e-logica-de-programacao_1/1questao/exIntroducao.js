51f7e528727ee00beabb78564dab62a31de1d180
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-05-19 18:40:27 -0300

    Segunda Questão Exercicio bl4.1

--- exercises/exercises/introducao-a-javascript-e-logica-de-programacao_1/1questao/exIntroducao.js
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_1/1questao/exIntroducao.js
@@ -1,15 +1,27 @@
-// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+// 1 Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
 // Adição (a + b)
 // Subtração (a - b)
 // Multiplicação (a * b)
 // Divisão (a / b)
 // Módulo (a % b)
 
-let a=20;
+// let a=20;
+// let b=50;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// 2 Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let a=30;
 let b=50;
 
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
+if(a>b){
+    console.log("a é maior que b");
+}
+else{
+    console.log("a menor que b");
+}
