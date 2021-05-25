6b103cf01c55bb8a8ebb629e5231ae60f5befd04
rene-t13-trybe <cleytonacompanha@gmail.com>
2021-05-25 19:58:58 -0300

    Exercicio bl4.4

--- exercises/exercises/introducao-a-javascript-e-logica-de-programacao_4/logica44parte1.js
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_4/logica44parte1.js
@@ -2,15 +2,29 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: sim, 
 };
 
-info['recorrente'] = 'Sim';
-
-// console.log(info);
+console.log(info);
 
 // 3-Faça um for/in que mostre todas as chaves do objeto.
 // Valor esperado no console:
 
-for (let key in info) {
-    console.log(info[key]);
-}
+// for (let key in info) {
+//     console.log(info[key]);
+// }
+
+// Margarida e Tio Patinhas
+//   Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+//   Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+//   Ambos recorrentes // Atenção para essa última linha!
+
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain Dells Four Color Comics #178',
+//     nota: 'O Ultimo MacPatinhas',
+//     recorrente: 'sim',
+// };
+
+// console.log(info);
+
